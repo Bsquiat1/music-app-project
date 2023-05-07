941f1b9320789325e5752b2f7162213d959992c9
Denis <denis.were@student.moringaschool.com>
2023-05-07 11:12:00 +0300

    search bar

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import { Error, Loader, SongCard} from '../components'
-import { useGetSongsBySearchQuery } from "../redux/services/shazamCore";
+import { useGetSongsBySearchQuery } from '../redux/services/shazamCore'
 
 const Search = () => {
   const { searchTerm } = useParams();
@@ -10,13 +10,17 @@ const Search = () => {
   const { data, isFetching, error} = useGetSongsBySearchQuery(searchTerm);
   const songs = data?.tracks?.hits.map((song) => song.track)
 
+  if (isFetching) return <Loader title="Loading top charts"/>;
+
+  if (error) return <Error />;
+
  return (
   <div className="flex flex-col">
     <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10" >Showing results for <span className="font-black">{searchTerm}</span></h2>
     <div className="flex flex-wrap sm:justify-start justify-center gap-8">
       {songs?.map((song, i) => (
         <SongCard 
-        key={song.id} 
+        key={song.key}  
         song={song} 
         activeSong={activeSong} 
         isPlaying={isPlaying}
